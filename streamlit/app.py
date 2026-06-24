ea2da782f992a1bf8e9ee97250e9c5c75ebc346f
Marcela Lozano <marcelalozano27@gmail.com>
2026-06-23 21:49:40 -0700

    Fix formatting issues in app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-F
+
 
 st.set_page_config(
     page_title="IRC Program Performance & Activity Planning Dashboard",
